35217184ed7f8678ed8a4931404e7d6074a08b98
dmartin110620 <danielmafr@unisabana.edu.co>
2025-11-26 00:13:23 -0500

    Initial commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Daniel Felipe Martin Franco on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
